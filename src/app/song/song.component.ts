009eacdb97d734d04bc76cb4f4b37e004442296c
Matthew Cho <chomb@uci.edu>
2024-06-07 16:52:30 -0700

    Song tile and updates

--- src/app/song/song.component.ts
+++ src/app/song/song.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./song.component.css']
 })
 export class SongComponent {
+  isLiked = false;
+  isPlaying = false;
 
+  toggleLike() {
+    this.isLiked = !this.isLiked;
+  }
+
+  togglePlayPause() {
+    this.isPlaying = !this.isPlaying;
+  }
 }
